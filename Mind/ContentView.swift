31acee111814cf1a6fdde770397fdb1dc481e385
AlexGrim12 <alejandrogrim12@gmail.com>
2026-04-19 15:37:25 -0600

    Initial Commit

---
+++ Mind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mind
+//
+//  Created by Alejandro Grimaldo on 19/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mind/MindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindApp.swift
+//  Mind
+//
+//  Created by Alejandro Grimaldo on 19/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
